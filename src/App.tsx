16276061aa2c03d7028b06b81961ab6ad911bd93
TimeSentinel <lance.stubblefield@gmail.com>
2025-03-05 18:47:09 -0800

    added link forwarding

--- src/App.tsx
+++ src/App.tsx
@@ -69,10 +69,10 @@ function App() {
                                         <img src="images/serious.png" alt="serious Lance"/>
                                     </div>
                                     <div>
-                                        <div className="linkTitle">Online<br/>Portfolio</div>
+                                        <div className="linkTitle">Online Portfolio</div>
                                         <br/>
                                         <div className="linkAddress">
-                                            <a target="_blank" href="https://lance.sol3.one">lance.sol3.one</a>
+                                            <a target="_blank" href="https://portfolio.sol3.one">lance.sol3.one</a>
                                         </div>
                                     </div>
                                 </div>
@@ -88,7 +88,7 @@ function App() {
                                 <div className="linkAddress">
                                     <a target="_blank"
                                        href="https://www.linkedin.com/in/lance-stubblefield-designer/">
-                                        www.linkedin.com/in/ <br/> lance-stubblefield-designer/
+                                        https://linkedin.sol3.one
                                     </a>
                                 </div>
                             </li>
@@ -101,7 +101,7 @@ function App() {
                                 </div>
                                 <div className="linkAddress">
                                     <a target="_blank" href="https://github.com/TimeSentinel">
-                                        github.com/TimeSentinel
+                                        https://git.sol3.one
                                     </a>
                                 </div>
                             </li>
@@ -114,7 +114,7 @@ function App() {
                                 </div>
                                 <div className="linkAddress">
                                     <a target="_blank" href="https://www.upwork.com/freelancers/~01b480b3ffdda5cdfa">
-                                        www.upwork.com/freelancers/~01b480b3ffdda5cdfa
+                                        https://upwork.sol3.one
                                     </a>
                                 </div>
                             </li>
